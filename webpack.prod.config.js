fdd8d5cc81a77e11675308df611a6f3d1f6e3591
Oscar Heimdahl <o.heimdahl@gmail.com>
2022-01-07 15:10:41 +0100

    this is the one

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -13,6 +13,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(scss)$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
