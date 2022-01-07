ca64a7f10dd874602fc3a72741a05806d0b02e29
Oscar Heimdahl <o.heimdahl@gmail.com>
2022-01-07 14:29:57 +0100

    Launch

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Chillin from './assets/chillin.jpg';
+
+const img = document.createElement('img');
+img.src = Chillin;
+
+document.querySelector('body').appendChild(img);
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+/** @type {import('@types/webpack').Configuration} */
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  devtool: false,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devServer: {},
+  plugins: [new HtmlWebpackPlugin()],
+};
